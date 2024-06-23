13354cb3210101ca8c6e798419cc56323a43e0ec
ruochenhua <ruochen.hua@gmail.com>
2024-06-23 11:54:43 +0800

    更新ignore文件；新增碰撞类hittable和球体字类sphere文件

---
+++ src/hittable.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+// 碰撞物体基类
+
+// 碰撞记录
+struct hit_record
+{
+    point3 p;
+    vec3 normal;
+    double t;
+};
+
+class hittable
+{
+public:
+    virtual ~hittable() = default;
+    virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+};
+
+#endif
---
+++ src/sphere.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable
+{
+public:
+    sphere(const point3& c, double r)
+        : center(c), radius(fmax(0, r))
+    { }
+
+    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override
+    {
+        const vec3 oc = center - r.origin();
+        // auto discriminant = b*b - 4*a*c;
+        // 二次方程有无解就是看这个式子能否开根号，若小于0则开不出根号没有实数解
+        auto a = r.direction().length_squared();
+        auto h = dot(r.direction(), oc);
+        auto c = oc.length_squared() - radius*radius;
+        auto discriminant = h*h - a*c;
+
+        if(discriminant < 0)
+        {
+            return false;
+        }
+
+        const auto sqrt_d = sqrt(discriminant);
+        auto root = h-sqrt_d / a;
+        // 这里检查两个可能的解，先检查离射线起点近的这一段
+        if(root < ray_tmin || ray_tmax <= root)
+        {
+            // 再检查远的这一段，都不符合那就没有碰撞的结果
+            root = h + sqrt_d / a;
+            if(root < ray_tmin || ray_tmax <= root)
+            {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.p = r.at(root);
+        rec.normal = (rec.p - center) / radius;
+
+        return true;
+    }
+
+private:
+    point3 center;
+    double radius;
+};
+
+#endif
