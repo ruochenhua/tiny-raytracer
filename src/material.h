5431aec7b25a7f6dd1d63063bd6749f5b667c68f
ruochenhua <ruochen.hua@gmail.com>
2024-06-28 09:57:18 +0000

    折射效果优化

--- src/material.h
+++ src/material.h
@@ -70,13 +70,38 @@ public:
         // 默认空气是1，从空气射入和射出的情况要考虑
         double ri = rec.b_front_face ? (1.0 / refraction_index) : refraction_index;
         vec3 unit_dir = unit_vector(r_in.direction());
-        vec3 refracted = refract(unit_dir, rec.normal, ri);
-        scattered = ray(rec.p, refracted);
+
+        // 在某种折射率的比值和入射角的情况下，光线入射是无法发生折射的，而是会发生反射
+        // A*sin(A_angle) = B*sin(B_angle)
+        // A / B * sin(A_angle) = sin(B_angle), A/B*sin(A_angle)范围在[-1,1]之间
+        double cos_theta = fmin(dot(-unit_dir, rec.normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+
+        bool not_refract = ri * sin_theta > 1.0 || ri * sin_theta < -1;
+        vec3 new_dir;
+        if(not_refract || reflectance(cos_theta, ri) > random_double())     // 为什么这里是个[0,1)的随机数作为判断？
+        {
+            new_dir = reflect(unit_dir, rec.normal);
+        }
+        else
+        {
+            new_dir = refract(unit_dir, rec.normal, ri);
+        }
+        scattered = ray(rec.p, new_dir);
         return true;
     }
 
 private:
     double refraction_index;
+
+    // Schlick近似：真实玻璃的反射是和入射角度相关的，可以用Schlick近似方法计算反射比
+    static double reflectance(double cosine, double refraction_index)
+    {
+        auto r0 = (1-refraction_index) / (1 + refraction_index);
+        r0 = r0*r0;
+        return r0 + (1-r0)*pow((1-cosine), 5);
+    }
+
 };
 
 #endif
