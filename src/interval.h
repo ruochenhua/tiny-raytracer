04290efd25887781594fd432145982308219a24f
ruochenhua <ruochen.hua@gmail.com>
2024-07-09 12:02:04 +0000

    box类和transform

--- src/interval.h
+++ src/interval.h
@@ -56,4 +56,12 @@ public:
 const interval interval::empty = interval();
 const interval interval::universe = interval(-infinity, infinity);
 
+interval operator+(const interval& ival, double displacement) {
+    return interval(ival.min + displacement, ival.max + displacement);
+}
+
+interval operator+(double displacement, const interval& ival) {
+    return ival + displacement;
+}
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -236,12 +236,22 @@ void render_cornell_box()
     world.add(make_shared<quad>(point3(555,555,555), vec3(-555,0,0), vec3(0,0,-555), white));
     world.add(make_shared<quad>(point3(0,0,555), vec3(555,0,0), vec3(0,555,0), white));
 
+    shared_ptr<hittable> box1 = box(point3(0,0,0), point3(165,330,165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    world.add(box1);
+
+    shared_ptr<hittable> box2 = box(point3(0,0,0), point3(165,165,165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+    world.add(box2);
+
     camera cam;
 
     cam.aspect_ratio      = 1.0;
-    cam.image_width       = 600;
-    cam.samples_per_pixel = 200;
-    cam.max_depth         = 50;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 50;
+    cam.max_depth         = 10;
     cam.background        = color(0,0,0);
 
     cam.vfov     = 40;
